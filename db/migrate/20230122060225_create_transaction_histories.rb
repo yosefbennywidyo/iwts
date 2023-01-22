c324f4c0809508424d8b947a0d71e2f769cd413b
Yosef Benny Widyokarsono <yosebennywidyo@gmail.com>
2023-01-22 14:03:40 +0800

    Update create transaction histories migration

--- db/migrate/20230122060225_create_transaction_histories.rb
+++ db/migrate/20230122060225_create_transaction_histories.rb
@@ -1,6 +1,13 @@
 class CreateTransactionHistories < ActiveRecord::Migration[7.0]
   def change
-    create_table :transaction_histories do |t|
+    create_table :transaction_histories, id: false, primary_key: :number do |t|
+      t.decimal :amount
+      t.string :type
+      t.string :owner_id
+      t.string :sender_id
+      t.string :receiver_id
+      t.string :number
+      t.string :description
 
       t.timestamps
     end
