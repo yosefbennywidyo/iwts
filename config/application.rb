d574e6ded888c85f2996d493969947001b3288bb
Yosef Benny Widyokarsono <yosebennywidyo@gmail.com>
2023-01-22 14:29:58 +0800

    Fix user, team, and stock models error as unitialized constant: - add models subfolder to autoload_path

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module Iwts
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
   end
 end
