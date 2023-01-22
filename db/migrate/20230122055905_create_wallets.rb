d4e6cf0b540592cfe135da44916dc91bac6ed115
Yosef Benny Widyokarsono <yosebennywidyo@gmail.com>
2023-01-22 14:02:16 +0800

    Update create wallet migration

--- db/migrate/20230122055905_create_wallets.rb
+++ db/migrate/20230122055905_create_wallets.rb
@@ -1,6 +1,9 @@
 class CreateWallets < ActiveRecord::Migration[7.0]
   def change
-    create_table :wallets do |t|
+    create_table :wallets, id: false, primary_key: :code do |t|
+      t.string :address
+      t.string :owner_id
+      t.decimal :balance, precision: 8, scale: 2, default: 0
 
       t.timestamps
     end
