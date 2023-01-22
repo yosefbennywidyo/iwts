42f8417874ec9a07e4e154c5f7925179dc2223a1
Yosef Benny Widyokarsono <yosebennywidyo@gmail.com>
2023-01-22 14:30:11 +0800

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  resources :transaction_histories
-  resources :wallets
-  resources :stocks
-  resources :teams
-  resources :users
+  resources :users, :teams, :stocks, param: :code do
+    resources :transaction_histories, param: :number, only: [:index] do
+      collection do
+        get 'debit', controller: 'transaction_histories', action: 'debit'
+        get 'credit', controller: 'transaction_histories', action: 'credit'
+        post 'new_debit', controller: 'transaction_histories', action: 'new_debit'
+        post 'new_credit', controller: 'transaction_histories', action: 'new_credit'
+      end
+    end
+  end
   get '*path' => redirect('/')
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
