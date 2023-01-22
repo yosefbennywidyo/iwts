6bc19d636536d623a85cea981495ad473c24474c
Yosef Benny Widyokarsono <yosebennywidyo@gmail.com>
2023-01-22 13:55:37 +0800

    Update create clients table: - remove clients table - create, users, teams, and stocks tables

--- db/migrate/20230122054918_create_clients.rb
+++ db/migrate/20230122054918_create_clients.rb
@@ -1,8 +1,36 @@
 class CreateClients < ActiveRecord::Migration[7.0]
   def change
-    create_table :clients do |t|
+    create_table :users do |t|
+      with_options null: false, id: false, primary_key: :code do
+        t.string :code
+        t.string :name
+        t.string :email
+      end
 
       t.timestamps
+      t.index :code, unique: true
+    end
+
+    create_table :teams do |t|
+      with_options null: false, id: false, primary_key: :code do
+        t.string :code
+        t.string :name
+        t.string :email
+      end
+
+      t.timestamps
+      t.index :code, unique: true
+    end
+
+    create_table :stocks do |t|
+      with_options null: false, id: false, primary_key: :code do
+        t.string :code
+        t.string :name
+        t.string :email
+      end
+
+      t.timestamps
+      t.index :code, unique: true
     end
   end
 end
