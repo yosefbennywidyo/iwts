b0e84936981a2092fd5ed360c7f15e7303c457fd
Yosef Benny Widyokarsono <yosebennywidyo@gmail.com>
2023-01-25 00:55:25 +0800

    Update home view test

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "home/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders a home#index" do
+    render
+
+    expect(rendered).to have_selector("h1", text: "Welcome to Internal Wallet Transactional System")
+
+    expect(rendered).to have_selector("p", text: "You can start your transaction by select this options")
+
+    expect(rendered).to have_selector("a", text: "Go to users")
+    expect(rendered).to have_selector("a", text: "Go to teams")
+    expect(rendered).to have_selector("a", text: "Go to stocks")
+  end
 end
